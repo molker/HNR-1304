a21665b296a2d3ca6593548dac438253c55c0303
Matthew Olker <molker@highpoint.edu>
2019-04-01 17:10:26 -0400

    check description
    got instant runoff python program working 
    got dataparse to fix up the real world examples
    added SF_2012d7 example working and in TestFiles

--- Source/instantRV.py
+++ Source/instantRV.py
@@ -3,21 +3,74 @@
 Instant Runoff Voting Method
 '''
 #function for pairwise comprison 
-def instant(currCan, rows, numVotes):
+def instant(a,b, rows, numVotes):
 	pairscore = [0, 0]
 	for x in range(len(rows[0])):
 		for y in range(len(rows)):
 			if rows[y][x] == a:
-				pairscore[0] += numVotes[x]
+				pairscore[0] += int(numVotes[x])
 				break
 			elif rows[y][x] == b:
-				pairscore[1] += numVotes[x]
+				pairscore[1] += int(numVotes[x])
 				break
 	if pairscore[0] > pairscore[1]:
 		return a
 	else:
 		return b
 
+def checkMajority(rows, numVotes, candidates):
+	fpVotes = []
+	total = 0
+	for x in range(len(candidates)):
+		fpVotes.append(0)
+	for x in range(len(numVotes)):
+		if rows[0][x] == "#":
+			found = False
+			for y in range(len(rows)):
+				if rows[y][x] != "#" and found != True:
+					fpVotes[ord(rows[y][x]) - 65] += int(numVotes[x])
+					found = True
+		else:
+			fpVotes[ord(rows[0][x]) - 65] += int(numVotes[x])
+	for x in fpVotes:
+		total += x
+	for x in range(len(fpVotes)):
+		check = float(fpVotes[x]) / float(total)
+		if check > 0.5:
+			return x
+	return -1
+
+def checkFP(rows, numVotes, candidates):
+	fpVotes = []
+	for x in range(len(candidates)):
+		fpVotes.append(0)
+	for x in range(len(numVotes)):
+		if rows[0][x] == "#":
+			found = False
+			for y in range(len(rows)):
+				if rows[y][x] != "#" and found != True:
+					fpVotes[ord(rows[y][x]) - 65] += int(numVotes[x])
+					found = True
+		else:
+			fpVotes[ord(rows[0][x]) - 65] += int(numVotes[x])
+	for x in range(len(fpVotes)):
+		if fpVotes[x] == 0:
+			fpVotes[x] = "#"
+	print fpVotes
+	lose = min(fpVotes)
+	print lose
+	for x in range(len(candidates)):
+		if fpVotes[x] == lose:
+			result = candidates[x]
+	return result
+
+def dump(rows, numVotes):
+	for x in range(len(numVotes)):
+		result = ""
+		result += numVotes[x] + " "
+		for y in range(len(rows)):
+			result += rows[y][x] + " "
+		print result + "\n"
 
 # Create all the different arrays
 candidates = []
@@ -25,19 +78,23 @@ numVotes = []
 rows = []
 scores = []
 
-# Get input describing the preference schedule 
-numRow = input()
-
-# Loops that will fill the arrays
-
-numVotes = raw_input()
-numVotes = numVotes.split(",")
-for n in range(len(numVotes)):
-	numVotes[n] = int(numVotes[n])
-
-for x in range(numRow):
-	row = raw_input()
-	rows.append(row)
+while True:
+	try:
+		line = raw_input()
+		line = line.split(" ")
+		vote = line[0]
+		row = line[1]
+		if len(row) < 3:
+			for x in range(3 - len(row)):
+				row += "#"
+		numVotes.append(vote)
+		if len(rows) < len(row):
+			for x in range(len(row)-len(rows)):
+				rows.append("");
+		for x in range(len(row)):
+			rows[x] += row[x]
+	except EOFError:
+		break
 
 for x in range(len(rows)):
 	for y in range(len(rows[x])):
@@ -49,23 +106,15 @@ for x in range(len(rows)):
 			candidates.append(rows[x][y])
 			scores.append(0)
 
-# Put each candidate against each other
-compared = []
-for x in candidates:
-	for y in candidates:
-		if x != y:
-			for z in compared:
-				if sorted(str(x+y)) == z:
-					break
-			else:
-				pwin = instant(x,y,rows,numVotes)
-				compared.append(sorted(str(x+y)))
-				scores[ord(pwin)-65] += 1
+while checkMajority(rows, numVotes, candidates) == -1:
+	losec = checkFP(rows, numVotes, candidates)
+	for x in range(len(rows)):
+		for y in range(len(rows[x])):
+			if rows[x][y] == losec:
+				tmp = list(rows[x])
+				tmp[y] = "#"
+				rows[x] = "".join(tmp)
 
-win = max(scores)
-# Print out the scores
-for x in range(len(candidates)):
-	if scores[x] == win:
-		winc = candidates[x]
-print winc + " Wins with " + str(win) + " points"
+win = candidates[checkMajority(rows, numVotes, candidates)]
+print win + " Wins"
 
---
+++ dataParse.cpp
@@ -0,0 +1,165 @@
+/*
+3/18/2019
+SF_dataParse.py
+Used for parsing the data from the SF election provided by Dr. Graham-Squire
+formats the data better and sorts it
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+# define NO_OF_CHARS 256 
+
+using namespace std;
+
+struct vote{
+	int numVote;
+	string order;
+};
+
+void dump(vector<vote>);
+
+void format(vector<vote>&, int numBalSpot);
+
+int vec_find(vector<vote> vec, string find);
+
+int vec_find(vector<vote> vec, string find, int n);
+
+void quickSort(vector<vote>&, int low, int high);
+
+void partition(vector<vote>&, int low, int high, int &pivotpoint);
+
+void exchange(vote&, vote&);
+
+char *removeDups(char *str);
+
+int main(){
+	int numCan, numBalSpot, nextVote;
+	vector<vote> voteOrder;
+	vote tmp;
+	cin >> numCan; 
+	cin >> numBalSpot;
+	while ( cin >> nextVote ){
+		if (nextVote != -1){
+			tmp.numVote = nextVote;
+			cin >> tmp.order;
+			char buff [tmp.order.size() + 1];
+			strcpy(buff, tmp.order.c_str());
+			tmp.order = removeDups(buff);
+			voteOrder.push_back(tmp);
+		}
+	}
+	format(voteOrder, numBalSpot);
+	quickSort(voteOrder, 0, voteOrder.size() );
+	//dump(voteOrder);
+	return 0;
+}
+
+void dump(vector<vote> vec){
+	int size = vec.size();
+	for (int i = 0; i < size; i++){
+		cout << vec[i].numVote << " " << vec[i].order << endl;
+	}
+}
+
+void format(vector<vote> &vec, int numBalSpot){
+	int size = vec.size(); 
+	string tmp;
+	for (int i = 0; i < size; i++){
+		if (vec_find(vec, vec[i].order, i) != -1){
+			vec[vec_find(vec, vec[i].order, i)].numVote += vec[i].numVote;
+			vec.erase(vec.begin()+i);
+		}
+		size = vec.size();
+	}
+}
+
+int vec_find(vector<vote> vec, string find){
+	int size = vec.size();
+	for (int i = 0; i < size; i++){
+		if (vec[i].order == find){
+			return i;
+		} else {
+			// cerr << "vec_find couldn't find requested string" << endl;
+		}
+	}
+	return -1;
+}
+
+int vec_find(vector<vote> vec, string find, int n){
+	int size = vec.size();
+	for (int i = 0; i < size; i++){
+		if (vec[i].order == find && i != n ){
+			return i;
+		} else {
+			// cerr << "vec_find couldn't find requested string" << endl;
+		}
+	}
+	return -1;
+}
+
+
+void quickSort(vector<vote> &vec, int low, int high){
+	int pivotpoint;
+	if (high > low){
+		partition(vec, low, high, pivotpoint);
+		quickSort(vec, low, pivotpoint - 1);
+		quickSort(vec, pivotpoint + 1, high);
+	}
+}
+
+void partition(vector<vote> &vec, int low, int high, int &pivotpoint){
+	int j = low ;
+	string pivot = vec[low].order;
+
+	for ( int i = low; i <= high - 1; i++ ){
+		if (pivot > vec[i].order){
+			j++;
+			exchange(vec[i], vec[j]);
+		}
+	}
+	pivotpoint = j;
+	exchange(vec[low], vec[pivotpoint]);
+}
+
+void exchange(vote &a, vote &b){
+	vote temp;
+	temp.numVote = a.numVote;
+	temp.order = a.order;
+
+	a.numVote = b.numVote;
+	a.order = b.order;
+
+	b.numVote = temp.numVote;
+	b.order = temp.order;
+}
+
+char *removeDups(char *str) 
+{ 
+  bool bin_hash[NO_OF_CHARS] = {0}; 
+  int ip_ind = 0, res_ind = 0; 
+  char temp;     
+  
+  /* In place removal of duplicate characters*/
+  while (*(str + ip_ind)) 
+  { 
+    temp = *(str + ip_ind); 
+    if (bin_hash[temp] == 0) 
+    { 
+        bin_hash[temp] = 1; 
+        *(str + res_ind) = *(str + ip_ind); 
+        res_ind++; 
+    } 
+    ip_ind++; 
+  }       
+  
+  /* After above step string is stringiittg. 
+     Removing extra iittg after string*/
+  *(str+res_ind) = '\0';    
+  
+  return str; 
+} 
